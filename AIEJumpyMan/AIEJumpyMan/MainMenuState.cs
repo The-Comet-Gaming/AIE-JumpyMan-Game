217721edae0c20fa8ddda51867f1257f2aad6012
The-Comet-Gaming <tjstew880@gmail.com>
2017-06-26 23:42:45 +1000

    26/06/2017

---
+++ AIEJumpyMan/AIEJumpyMan/MainMenuState.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace AIEJumpyMan
+{
+    public class MainMenuState : AIE.State
+    {
+        bool isLoaded = false;
+        SpriteFont font = null;
+        public int menuSelection = 1;
+        KeyboardState oldState;
+
+        public MainMenuState() : base()
+        {
+
+        }
+
+        public override void Update(ContentManager content, GameTime gameTime)
+        {
+            if (isLoaded == false)
+            {
+                isLoaded = true;
+                font = content.Load<SpriteFont>("Agency_FB");
+                oldState = Keyboard.GetState();
+            }
+
+            KeyboardState newState = Keyboard.GetState();
+
+            if ("MAINMENU" == AIE.StateManager.StateStackCurrent)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Enter) == true && menuSelection == 1)
+                {
+                    AIE.StateManager.ChangeState("LEVELSELECT");
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Enter) == true && menuSelection == 2)
+                {
+                    AIE.StateManager.ChangeState("HOW2PLAYMENU");
+                }
+            }
+
+            if ("LEVELSELECT" == AIE.StateManager.StateStackCurrent)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Enter) == true && menuSelection == 1)
+                {
+                    // checking the old state ensures that we only process new key
+                    //presses (incase the key was held down from the last state
+                    if (oldState.IsKeyDown(Keys.Enter) == false)
+                    {
+                        AIE.StateManager.ChangeState("LOADING1");
+                    }                        
+                }
+
+                if (Keyboard.GetState().IsKeyDown(Keys.Enter) == true && menuSelection == 2)
+                {
+                    // checking the old state ensures that we only process new key
+                    //presses (incase the key was held down from the last state
+                    if (oldState.IsKeyDown(Keys.Enter) == false)
+                    {
+                        AIE.StateManager.ChangeState("LOADING2");
+                        
+                    }
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.Back) == true)
+                {
+                    // checking the old state ensures that we only process new key
+                    //presses (incase the key was held down from the last state
+                    if (oldState.IsKeyDown(Keys.Back) == false)
+                    {
+                        AIE.StateManager.ChangeState("MAINMENU");
+                    }
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) == true && menuSelection > 1)
+            {
+                menuSelection -= 1;         
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) == true && menuSelection < 2)
+            {
+                menuSelection += 1;
+            }
+            oldState = newState;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if ("MAINMENU" == AIE.StateManager.StateStackCurrent)
+            {
+                spriteBatch.Begin();
+                spriteBatch.DrawString(font, "JUMPY MC JUMP MAN", new Vector2(20, 20), Color.White);
+                if (menuSelection == 1)
+                {
+                    spriteBatch.DrawString(font, "Select Level", new Vector2(20, 60), Color.Silver);
+                }
+                else
+                {
+                    spriteBatch.DrawString(font, "Select Level", new Vector2(20, 60), Color.White);
+                }
+                if (menuSelection == 2)
+                {
+                    spriteBatch.DrawString(font, "How to Play", new Vector2(20, 80), Color.Silver);
+                }
+                else
+                {
+                    spriteBatch.DrawString(font, "How to Play", new Vector2(20, 80), Color.White);
+                }
+                spriteBatch.DrawString(font, "Press ESC to Close Game", new Vector2(20, 110), Color.White);
+                spriteBatch.End();
+            }
+            
+            if ("LEVELSELECT" == AIE.StateManager.StateStackCurrent)
+            {
+                spriteBatch.Begin();
+                spriteBatch.DrawString(font, "LEVEL SELECT", new Vector2(20, 20), Color.White);
+                if (menuSelection == 1)
+                {
+                    spriteBatch.DrawString(font, "Level 1", new Vector2(20, 60), Color.Silver);
+                }
+                else
+                {
+                    spriteBatch.DrawString(font, "Level 1", new Vector2(20, 60), Color.White);
+                }
+                if (menuSelection == 2)
+                {
+                    spriteBatch.DrawString(font, "Level 2", new Vector2(20, 80), Color.Silver);
+                }
+                else
+                {
+                    spriteBatch.DrawString(font, "level 2", new Vector2(20, 80), Color.White);
+                }
+                spriteBatch.DrawString(font, "Press Backspace to return to Main Menu", new Vector2(20, 110), Color.White);
+                spriteBatch.End();
+            }            
+        }
+
+        public override void CleanUp()
+        {
+            font = null;
+            isLoaded = false;
+        }
+    }
+}
---
+++ AIEJumpyMan/AIEJumpyMan/SplashState.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace AIEJumpyMan
+{
+    class SplashState : AIE.State
+    {
+        SpriteFont font = null;
+        float timer = 3;
+
+        public SplashState() : base()
+        {
+
+        }
+
+        public override void Update(ContentManager content, GameTime gameTime)
+        {
+            if (font == null)
+            {
+                font = content.Load<SpriteFont>("Agency_FB");
+            }
+
+            timer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            
+
+            if (timer <= 0 && "LOADING1" == AIE.StateManager.StateStackCurrent)
+            {
+                AIE.StateManager.ChangeState("GAME");
+                timer = 3;
+            }
+
+            if (timer <= 0 && "LOADING2" == AIE.StateManager.StateStackCurrent)
+            {
+                AIE.StateManager.ChangeState("LEVEL2");
+                timer = 3;
+            }
+
+            else if (timer <= 0)
+            {
+                AIE.StateManager.ChangeState("MAINMENU");
+                timer = 3;
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, "Loading...", new Vector2(20, 20), Color.White);
+            spriteBatch.End();
+        }
+
+        public override void CleanUp()
+        {
+            font = null;
+            timer = 6;
+        }
+    }
+}
